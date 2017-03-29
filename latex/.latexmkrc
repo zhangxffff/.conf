998f19d7c2a4861bacf0ac23a41007fc0ef7970c
Xiaofeng Zhang <zhangxffff@gmail.com>
2017-03-30 02:46:48 +0800

    add latexmk

---
+++ latex/.latexmkrc
@@ -0,0 +1,2 @@
+$pdflatex="xelatex -synctex=1 %O %S";
+$out_dir="/tmp/";
--- link.sh
+++ link.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ln -s "`pwd`/vim/vimrc" "$HOME/.vimrc"
 ln -s "`pwd`/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -s "`pwd`/latex/.latexmkrc" "$HOME/.latexmkrc"
