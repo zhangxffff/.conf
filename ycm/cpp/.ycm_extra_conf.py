8f79b76e39d543a01c778731615d776a78057b7d
Xiaofeng Zhang <zhangxffff@gmail.com>
2017-11-07 16:37:47 +0800

    change some ycm config

--- ycm/cpp/.ycm_extra_conf.py
+++ ycm/cpp/.ycm_extra_conf.py
@@ -38,9 +38,10 @@ flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
-'-Wc++98-compat',
+#'-Wc++98-compat',
 '-Wno-long-long',
 '-Wno-variadic-macros',
+'-Wno-sign-compare',
 '-fexceptions',
 '-DNDEBUG',
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
