504be7d127727cf365630e24d017f741b9e26f11
Xiaofeng Zhang <zhangxffff@gmail.com>
2017-11-21 23:54:26 +0800

    add clone vundle

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s "`pwd`/vim/vimrc" "$HOME/.vimrc"
 ln -s "`pwd`/tmux/tmux.conf" "$HOME/.tmux.conf"
 ln -s "`pwd`/latex/.latexmkrc" "$HOME/.latexmkrc"
